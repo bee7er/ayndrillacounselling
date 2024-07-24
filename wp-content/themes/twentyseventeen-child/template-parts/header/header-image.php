ba5a0f618fc1386b9532f47d86a53832758e06cc
bee7er <contact_bee@yahoo.com>
2024-07-24 15:35:07 +0000

    First commit

---
+++ wp-content/themes/twentyseventeen-child/template-parts/header/header-image.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Displays header media
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since Twenty Seventeen 1.0
+ * @version 1.0
+ */
+
+?>
+<div class="custom-header">
+
+<!-- //BEE 20240723 -->
+	<img src="/wp-content/uploads/2024/07/clouds_header.jpg" width="100%" height="340" style="xxxmax-height: 540px !important;" alt="">
+
+</div><!-- .custom-header -->
